30bc4809a62ea963937934513e684e0d61412f15
Valeria Belyaeva <62827928+ValeriaBelyaeva@users.noreply.github.com>
2025-05-30 14:44:51 +0300

    upedate (fixing docker)

--- src/python_tools/__init__.py
+++ src/python_tools/__init__.py
@@ -1,6 +1,10 @@
+# This file is intentionally left empty.
+
 """
 Development tools package
 """
 
 __version__ = "1.0.0"
-__author__ = "Tetris Team" 
+__author__ = "Tetris Team"
+
+"""This package is intentionally kept minimal.""" 
